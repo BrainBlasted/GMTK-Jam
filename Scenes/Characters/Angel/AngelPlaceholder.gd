e5e89dcef89167e2ca168eba76ac8eab379dc98b
BrainBlasted <brainblastedmods@gmail.com>
2017-07-16 03:27:26 -0400

    Established how movement works; tried to figure out how to move camera

--- Scenes/Characters/Angel/AngelPlaceholder.gd
+++ Scenes/Characters/Angel/AngelPlaceholder.gd
@@ -1,13 +1,27 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var speed = 70.0
+var velocity = Vector2()
 
 func _fixed_process(delta):
-	move( Vector2(0,-1))
+	# move( Vector2(0,-1))
+	velocity.y -= delta * speed
+	var motion = delta * velocity
+	move(motion)
+	if Input.is_key_pressed(KEY_DOWN):
+		_slow()
+	if Input.is_key_pressed(KEY_UP):
+		_speedup()	
+func _slow():
+	# Slows the character
+	speed = 50.0
+func _speedup():
+	# Slows the character
+	speed = 300.0	
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	set_fixed_process(true)
+
 	pass
+
