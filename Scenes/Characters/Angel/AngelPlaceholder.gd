70075cd4700eaf03ceeeacb5da151499b82939ea
BrainBlasted <brainblastedmods@gmail.com>
2017-07-17 03:10:25 -0400

    Implemented dashbuttons; No clue how to stop

--- Scenes/Characters/Angel/AngelPlaceholder.gd
+++ Scenes/Characters/Angel/AngelPlaceholder.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var speed = 70.0
 var velocity = Vector2()
-var dashspeed = 40.0
+const dashspeed = 100.0
 
 func _fixed_process(delta):
 	# move( Vector2(0,-1))
@@ -13,19 +13,27 @@ func _fixed_process(delta):
 		_slow()
 	elif Input.is_key_pressed(KEY_UP):
 		_speedup()
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x = dashspeed
+	elif Input.is_key_pressed(KEY_LEFT):
+		velocity.x = -dashspeed
 #	elif Input.is_key_pressed(KEY_LEFT):
 #		_dash_left()
+
 func _slow():
 	# Slows the character
 	speed = 50.0
+
 func _speedup():
 	# Slows the character
 	speed = 300.0	
+
 #func _dash_left():
 #	# Left dash
 #	velocity.x -= dashspeed
 #	var motionx = int(dashspeed) * 1
 #	move(motionx)
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
